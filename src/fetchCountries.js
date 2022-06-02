8dd1bd372fd15282f06e82a1f1e45d031b106e12
Hanna-Pylypenko <pilipenkog679@gmail.com>
2022-06-02 11:00:06 +0300

    fgs

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,6 +2,9 @@ export function fetchCountries(name) {
   const fetchRef = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
 
   return fetch(fetchRef).then(response => {
+    if (response.status === 404) {
+      throw new Error();
+    }
     return response.json();
   });
 }
